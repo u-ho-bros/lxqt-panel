13fd3e6249b1f79b501f412f434ab8922d233459
Ryuho Kumagai <u_ho_@msn.com>
2015-10-12 19:30:05 +0900

    lxqt-panel: Update japanese translation.

--- panel/translations/lxqt-panel_ja.ts
+++ panel/translations/lxqt-panel_ja.ts
@@ -2,223 +2,313 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ja">
 <context>
+    <name>AddPluginDialog</name>
+    <message>
+        <location filename="../config/addplugindialog.ui" line="14"/>
+        <source>Add Plugins</source>
+        <translation>プラグインの追加</translation>
+    </message>
+    <message>
+        <location filename="../config/addplugindialog.ui" line="22"/>
+        <source>Search:</source>
+        <translation>検索:</translation>
+    </message>
+    <message>
+        <location filename="../config/addplugindialog.ui" line="98"/>
+        <source>Add Widget</source>
+        <translation>ウィジェットの追加</translation>
+    </message>
+    <message>
+        <location filename="../config/addplugindialog.ui" line="105"/>
+        <source>Close</source>
+        <translation>閉じる</translation>
+    </message>
+    <message>
+        <location filename="../config/addplugindialog.cpp" line="114"/>
+        <source>(only one instance can run at a time)</source>
+        <translation>(インスタンスは一度に1つだけ実行することができます)</translation>
+    </message>
+</context>
+<context>
     <name>ConfigPanelDialog</name>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="80"/>
-        <location filename="../config/configpaneldialog.cpp" line="86"/>
+        <location filename="../config/configpaneldialog.cpp" line="31"/>
         <source>Configure Panel</source>
-        <translation>パネルを設定</translation>
+        <translation>パネルの設定</translation>
+    </message>
+    <message>
+        <location filename="../config/configpaneldialog.cpp" line="38"/>
+        <source>Panel</source>
+        <translation>パネル</translation>
+    </message>
+    <message>
+        <location filename="../config/configpaneldialog.cpp" line="42"/>
+        <source>Widgets</source>
+        <translation>ウィジェット</translation>
     </message>
 </context>
 <context>
     <name>ConfigPanelWidget</name>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="20"/>
+        <location filename="../config/configpanelwidget.ui" line="20"/>
         <source>Configure panel</source>
-        <translation>パネルを設定</translation>
+        <translation>パネルの設定</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="32"/>
+        <location filename="../config/configpanelwidget.ui" line="44"/>
         <source>Size</source>
         <translation>大きさ</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="41"/>
-        <source>Size:</source>
-        <translation>幅:</translation>
-    </message>
-    <message>
-        <location filename="../config/configpaneldialog.ui" line="48"/>
-        <location filename="../config/configpaneldialog.ui" line="82"/>
-        <source> px</source>
-        <translation> ピクセル</translation>
-    </message>
-    <message>
-        <location filename="../config/configpaneldialog.ui" line="58"/>
-        <source>Icon size:</source>
-        <translation>アイコンの大きさ:</translation>
-    </message>
-    <message>
-        <location filename="../config/configpaneldialog.ui" line="65"/>
-        <source>Length:</source>
-        <translation>長さ:</translation>
-    </message>
-    <message>
-        <location filename="../config/configpaneldialog.ui" line="110"/>
+        <location filename="../config/configpanelwidget.ui" line="68"/>
         <source>&lt;p&gt;Negative pixel value sets the panel length to that many pixels less than available screen space.&lt;/p&gt;&lt;p/&gt;&lt;p&gt;&lt;i&gt;E.g. &quot;Length&quot; set to -100px, screen size is 1000px, then real panel length will be 900 px.&lt;/i&gt;&lt;/p&gt;</source>
         <translation>&lt;p&gt;負のピクセル値を設定すると、スクリーンの最大領域からその値を差し引いた長さになります。&lt;/p&gt;&lt;p/&gt;&lt;p&gt;&lt;i&gt;例: スクリーンの大きさが 1000 ピクセルである場合に -100 ピクセルを設定すると、パネルの長さは 900 ピクセルになります。&lt;/i&gt;&lt;/p&gt;</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="124"/>
+        <location filename="../config/configpanelwidget.ui" line="81"/>
+        <source>Size:</source>
+        <translation>幅:</translation>
+    </message>
+    <message>
+        <location filename="../config/configpanelwidget.ui" line="88"/>
+        <source>Length:</source>
+        <translation>長さ:</translation>
+    </message>
+    <message>
+        <location filename="../config/configpanelwidget.ui" line="96"/>
         <source>%</source>
         <translation>%</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="129"/>
+        <location filename="../config/configpanelwidget.ui" line="101"/>
         <source>px</source>
         <translation>ピクセル</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="140"/>
-        <source>Rows count:</source>
-        <translation>列の数</translation>
+        <location filename="../config/configpanelwidget.ui" line="109"/>
+        <location filename="../config/configpanelwidget.ui" line="153"/>
+        <source> px</source>
+        <translation> ピクセル</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="172"/>
+        <location filename="../config/configpanelwidget.ui" line="166"/>
+        <source>Icon size:</source>
+        <translation>アイコンの大きさ:</translation>
+    </message>
+    <message>
+        <location filename="../config/configpanelwidget.ui" line="173"/>
+        <source>Rows count:</source>
+        <translation>列の数:</translation>
+    </message>
+    <message>
+        <location filename="../config/configpanelwidget.ui" line="202"/>
         <source>Alignment &amp;&amp; position</source>
         <translation>位置寄せと場所</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="182"/>
-        <location filename="../config/configpaneldialog.cpp" line="234"/>
+        <location filename="../config/configpanelwidget.ui" line="208"/>
+        <source>Position:</source>
+        <translation>場所:</translation>
+    </message>
+    <message>
+        <location filename="../config/configpanelwidget.ui" line="218"/>
+        <source>Alignment:</source>
+        <translatorcomment>動作確認後、再翻訳</translatorcomment>
+        <translation>位置寄せ:</translation>
+    </message>
+    <message>
+        <location filename="../config/configpanelwidget.ui" line="244"/>
+        <location filename="../config/configpanelwidget.cpp" line="190"/>
         <source>Left</source>
         <translation>左寄せ</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="187"/>
-        <location filename="../config/configpaneldialog.cpp" line="235"/>
-        <location filename="../config/configpaneldialog.cpp" line="241"/>
+        <location filename="../config/configpanelwidget.ui" line="249"/>
+        <location filename="../config/configpanelwidget.cpp" line="191"/>
+        <location filename="../config/configpanelwidget.cpp" line="197"/>
         <source>Center</source>
         <translation>中央</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="192"/>
-        <location filename="../config/configpaneldialog.cpp" line="236"/>
+        <location filename="../config/configpanelwidget.ui" line="254"/>
+        <location filename="../config/configpanelwidget.cpp" line="192"/>
         <source>Right</source>
         <translation>右寄せ</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="200"/>
-        <source>Alignment:</source>
-        <translation>位置寄せ:</translation>
-    </message>
-    <message>
-        <location filename="../config/configpaneldialog.ui" line="210"/>
-        <source>Position:</source>
-        <translation type="unfinished">場所:</translation>
-    </message>
-    <message>
-        <location filename="../config/configpaneldialog.ui" line="217"/>
+        <location filename="../config/configpanelwidget.ui" line="284"/>
         <source>Auto-hide</source>
-        <translation type="unfinished"></translation>
+        <translation>自動的に隠す</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="233"/>
-        <source>Styling</source>
-        <translation>見た目</translation>
+        <location filename="../config/configpanelwidget.ui" line="303"/>
+        <source>Custom styling</source>
+        <translation>カスタムスタイル</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="239"/>
-        <source>Custom font color:</source>
-        <translation>フォントの色を変更:</translation>
+        <location filename="../config/configpanelwidget.ui" line="324"/>
+        <source>Font color:</source>
+        <translation>フォントの色:</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="246"/>
-        <source>Custom background image:</source>
-        <translation>背景画像を指定</translation>
+        <location filename="../config/configpanelwidget.ui" line="364"/>
+        <source>Background color:</source>
+        <translation>背景の色:</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="260"/>
-        <source>Custom background color:</source>
-        <translation>背景の色を変更:</translation>
+        <location filename="../config/configpanelwidget.ui" line="403"/>
+        <source>Background opacity:</source>
+        <translation>背景の不透過率:</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.ui" line="351"/>
-        <source>Opacity</source>
-        <translation>透明度</translation>
+        <location filename="../config/configpanelwidget.ui" line="435"/>
+        <source>&lt;small&gt;Compositing is required for panel transparency.&lt;/small&gt;</source>
+        <translation>&lt;small&gt;パネルの透過には、コンポジット機能が必要です。&lt;/small&gt;</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="200"/>
+        <location filename="../config/configpanelwidget.ui" line="463"/>
+        <source>Background image:</source>
+        <translation>背景の画像:</translation>
+    </message>
+    <message>
+        <location filename="../config/configpanelwidget.cpp" line="156"/>
         <source>Top of desktop</source>
         <translation>デスクトップの上</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="201"/>
+        <location filename="../config/configpanelwidget.cpp" line="157"/>
         <source>Left of desktop</source>
         <translation>デスクトップの左</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="202"/>
+        <location filename="../config/configpanelwidget.cpp" line="158"/>
         <source>Right of desktop</source>
         <translation>デスクトップの右</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="203"/>
+        <location filename="../config/configpanelwidget.cpp" line="159"/>
         <source>Bottom of desktop</source>
         <translation>デスクトップの下</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="212"/>
+        <location filename="../config/configpanelwidget.cpp" line="168"/>
         <source>Top of desktop %1</source>
-        <translation>デスクトップ %1 の上</translation>
+        <translation>%1 デスクトップの上</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="213"/>
+        <location filename="../config/configpanelwidget.cpp" line="169"/>
         <source>Left of desktop %1</source>
-        <translation>デスクトップ %1 の左</translation>
+        <translation>%1 デスクトップの左</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="214"/>
+        <location filename="../config/configpanelwidget.cpp" line="170"/>
         <source>Right of desktop %1</source>
-        <translation>デスクトップ %1 の右</translation>
+        <translation>%1 デスクトップの右</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="215"/>
+        <location filename="../config/configpanelwidget.cpp" line="171"/>
         <source>Bottom of desktop %1</source>
-        <translation>デスクトップ %1 の下</translation>
+        <translation>%1 デスクトップの下</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="240"/>
+        <location filename="../config/configpanelwidget.cpp" line="196"/>
         <source>Top</source>
         <translation>上</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="242"/>
+        <location filename="../config/configpanelwidget.cpp" line="198"/>
         <source>Bottom</source>
         <translation>下</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="397"/>
-        <location filename="../config/configpaneldialog.cpp" line="411"/>
+        <location filename="../config/configpanelwidget.cpp" line="354"/>
+        <location filename="../config/configpanelwidget.cpp" line="370"/>
         <source>Pick color</source>
-        <translation>色を選ぶ</translation>
+        <translation>色を選択</translation>
     </message>
     <message>
-        <location filename="../config/configpaneldialog.cpp" line="431"/>
+        <location filename="../config/configpanelwidget.cpp" line="388"/>
+        <source>Pick image</source>
+        <translation>画像の選択</translation>
+    </message>
+    <message>
+        <location filename="../config/configpanelwidget.cpp" line="388"/>
         <source>Images (*.png *.gif *.jpg)</source>
-        <translation>画像 (*.png *.gif *.jpg)</translation>
+        <translation>イメージファイル (*.png *.gif *.jpg)</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigPluginsWidget</name>
+    <message>
+        <location filename="../config/configpluginswidget.ui" line="14"/>
+        <source>Configure Plugins</source>
+        <translation>プラグインの設定</translation>
+    </message>
+    <message>
+        <location filename="../config/configpluginswidget.ui" line="84"/>
+        <source>Note: changes made in this page cannot be reset.</source>
+        <translation>注意：このページで行う変更は、リセットボタンでリセットできません。</translation>
+    </message>
+    <message>
+        <location filename="../config/configpluginswidget.ui" line="112"/>
+        <source>Move up</source>
+        <translation>上に移動</translation>
+    </message>
+    <message>
+        <location filename="../config/configpluginswidget.ui" line="115"/>
+        <location filename="../config/configpluginswidget.ui" line="129"/>
+        <location filename="../config/configpluginswidget.ui" line="150"/>
+        <location filename="../config/configpluginswidget.ui" line="164"/>
+        <location filename="../config/configpluginswidget.ui" line="185"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../config/configpluginswidget.ui" line="126"/>
+        <source>Move down</source>
+        <translation>下に移動</translation>
+    </message>
+    <message>
+        <location filename="../config/configpluginswidget.ui" line="147"/>
+        <source>Add</source>
+        <translation>追加</translation>
+    </message>
+    <message>
+        <location filename="../config/configpluginswidget.ui" line="161"/>
+        <source>Remove</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="../config/configpluginswidget.ui" line="182"/>
+        <source>Configure</source>
+        <translation>設定</translation>
     </message>
 </context>
 <context>
     <name>LXQtPanel</name>
     <message>
-        <location filename="../lxqtpanel.cpp" line="623"/>
-        <source>Add Panel Widgets</source>
-        <translation>パネルウィジェットを追加</translation>
-    </message>
-    <message>
-        <location filename="../lxqtpanel.cpp" line="951"/>
-        <location filename="../lxqtpanel.cpp" line="970"/>
+        <location filename="../lxqtpanel.cpp" line="912"/>
+        <location filename="../lxqtpanel.cpp" line="931"/>
         <source>Panel</source>
         <translation>パネル</translation>
     </message>
     <message>
-        <location filename="../lxqtpanel.cpp" line="973"/>
-        <source>Configure Panel...</source>
+        <location filename="../lxqtpanel.cpp" line="934"/>
+        <source>Configure Panel</source>
         <translation>パネルの設定</translation>
     </message>
     <message>
-        <location filename="../lxqtpanel.cpp" line="978"/>
-        <source>Add Panel Widgets...</source>
-        <translation>ウィジェットを追加</translation>
+        <location filename="../lxqtpanel.cpp" line="939"/>
+        <source>Manage Widgets</source>
+        <translation>ウィジェットの管理</translation>
     </message>
     <message>
-        <location filename="../lxqtpanel.cpp" line="984"/>
+        <location filename="../lxqtpanel.cpp" line="945"/>
         <source>Add Panel</source>
         <translation>パネルを追加</translation>
     </message>
     <message>
-        <location filename="../lxqtpanel.cpp" line="991"/>
+        <location filename="../lxqtpanel.cpp" line="952"/>
         <source>Remove Panel</source>
         <translation>パネルを削除</translation>
     </message>
@@ -226,19 +316,32 @@
 <context>
     <name>Plugin</name>
     <message>
-        <location filename="../plugin.cpp" line="395"/>
+        <location filename="../plugin.cpp" line="403"/>
         <source>Configure &quot;%1&quot;</source>
         <translation>&quot;%1&quot; を設定</translation>
     </message>
     <message>
-        <location filename="../plugin.cpp" line="400"/>
+        <location filename="../plugin.cpp" line="408"/>
         <source>Move &quot;%1&quot;</source>
         <translation>&quot;%1&quot; を移動</translation>
     </message>
     <message>
-        <location filename="../plugin.cpp" line="408"/>
+        <location filename="../plugin.cpp" line="416"/>
         <source>Remove &quot;%1&quot;</source>
         <translation>&quot;%1&quot; を削除</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../lxqtpanelapplication.cpp" line="52"/>
+        <source>Use alternate configuration file.</source>
+        <translation>別の設定ファイルを使用します。</translation>
+    </message>
+    <message>
+        <location filename="../lxqtpanelapplication.cpp" line="53"/>
+        <source>Configuration file</source>
+        <translation>設定ファイル</translation>
     </message>
 </context>
 </TS>
